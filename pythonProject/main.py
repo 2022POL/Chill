1715a71eddfe10de1edf830fe080ff17a85e1f63
Jueun-Shin <jes8708@gmail.com>
2022-11-24 13:56:18 +0900

    s누르면 봄 팔레트, e 누르면 여름 팔레트, f 누르면 가을 팔레트, w 누르면 겨울 팔레트 나오게 해뒀고, 팔레트 창 닫고 스케치 채색하게 하려면 아무 버튼이나 누르면 됨.

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -7,13 +7,14 @@ import pyautogui as pag
 
 def colorClick(event, x, y, flags, param):
       global last, img_color
+
       if event == cv2.EVENT_LBUTTONDOWN:
           print(img_color[y, x])
           color = img_color[y, x]
 
           one_pixel = np.uint8(color)
           last = one_pixel
-          # cv2.destroyWindow('img_color')
+
 
 # 마우스 이벤트 처리 함수
 def onMouse(event, x, y, flags, param):
@@ -28,38 +29,41 @@ def onMouse(event, x, y, flags, param):
 #     if event == cv2.EVENT_LBUTTONDOWN:
 #         cv2.waitKey(0)
 #         cv2.destroyAllWindows()
-def chooseColor():
+
+
+def chooseColor(pal):
     global last, img_color
 
     cv2.namedWindow('img_color')
     cv2.setMouseCallback('img_color', colorClick)
 
-    img_color = cv2.imread('pallette/fall.jpg')
+    img_color = cv2.imread(pal)
     cv2.imshow('img_color', img_color)
     key = cv2.waitKey(0)&0xFF # 이거 안 하면 에러뜸
 
-    #cv2.destroyWindow('img_color')
+    cv2.destroyWindow('img_color')
     a = []
     for i in range(0, 3):
         a.append(last[i].item())
     return tuple(a)
 
-img = cv2.imread('Sketch/flower3.jpg')
-image_gray = cv2.imread('Sketch/flower3.jpg', cv2.IMREAD_GRAYSCALE)
 
-b,g,r = cv2.split(img)
-image2 = cv2.merge([r,g,b])
+img = cv2.imread('Sketch/dori.jpg')
+image_gray = cv2.imread('Sketch/dori.jpg', cv2.IMREAD_GRAYSCALE)
 
-blur = cv2.GaussianBlur(image_gray, ksize=(5,5), sigmaX=0)
+b, g, r = cv2.split(img)
+image2 = cv2.merge([r, g, b])
+
+blur = cv2.GaussianBlur(image_gray, ksize=(5, 5), sigmaX=0)
 ret, thresh1 = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY)
 
 edged = cv2.Canny(blur, 10, 250)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, ))
 closed = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
 
 contours, hierarchy = cv2.findContours(closed.copy(),cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 total = 0
-contours_image = cv2.drawContours(img, contours, -1, (0,255,0), 7)
+contours_image = cv2.drawContours(img, contours, -1, (0, 255, 0), 7)
 
 rows, cols = img.shape[:2]
 # 마스크 생성, 원래 이미지 보다 2픽셀 크게 ---①
@@ -72,7 +76,7 @@ mask = np.zeros((rows+2, cols+2), np.uint8)
 # newVal = chooseColor()
 
 # 최소 최대 차이 값 ---③
-loDiff, upDiff = (10,10,10), (10,10,10)
+loDiff, upDiff = (10, 10, 10), (10, 10, 10)
 
 
 # 화면 출력
@@ -83,15 +87,15 @@ cv2.setMouseCallback('img', onMouse)
 while True:
     key = cv2.waitKey(0)
 
-    if key==ord('c'):
-        newVal = chooseColor()
-    elif key == ord('a'):
-        cv2.destroyWindow('img_color')
+    if key == ord('s'):
+        newVal = chooseColor('pallette/spring.jpg')
+    elif key == ord('e'):
+        newVal = chooseColor('pallette/summer.jpg')
+    elif key == ord('f'):
+        newVal = chooseColor('pallette/fall.jpg')
+    elif key == ord('w'):
+        newVal = chooseColor('pallette/winter.jpg')
     elif key == ord('v'):
         cv2.imwrite("result/colored.png", img)
         cv2.destroyAllWindows()
         break
-
-# cv2.destroyAllWindows()
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
